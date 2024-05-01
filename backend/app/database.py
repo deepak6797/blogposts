2a9d18ceb4e48b7bf26d02a1d98f4549b71b6c45
Deepak Gupta <deepakg6797@gmail.com>
2024-05-01 10:39:09 +0545

    Frontend Appearing

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://username:password@localhost/dbname"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://deepak:Deepak@localhost/blog_post"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,38 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import BlogPostList from './components/BlogPostList';
 import BlogPostDetails from './components/BlogPostDetails';
 import BlogPostForm from './components/BlogPostForm';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div>
+        <h1>My Blog</h1>
         <Switch>
-          <Route path="/" exact component={BlogPostList} />
+          {/* Routes for managing blog posts */}
           <Route path="/posts/:postId" component={BlogPostDetails} />
           <Route path="/create" component={BlogPostForm} />
+          <Route path="/postlist" exact component={BlogPostList} />
+          <Route path="/postlist/:postId" component={BlogPostDetails} />
+          <Route path="/postcreate" component={BlogPostForm} />
+          {/* Default route for main blog page */}
+          <Route path="/" exact render={() => (
+            <div>
+              <BlogPostList />
+              <hr />
+              <h2>Create New Post</h2>
+              <BlogPostForm />
+              <hr />
+              <h2>Post Details</h2>
+              <BlogPostDetails postId={1} /> {/* Pass the actual post ID */}
+            </div>
+          )} />
         </Switch>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
